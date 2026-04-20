764fc142a0fb0f37f214dfc754244c8e4fb9b30c
Khine Zar Hein <khinezarhein1@gmail.com>
2026-04-20 14:52:38 -0700

    chore: remove nested duplicate folders

--- burmalingo/frontend/src/App.tsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="min-h-screen bg-cream flex items-center justify-center">
-      <h1 className="font-serif text-4xl text-bark">BurmaLingo</h1>
-    </div>
-  )
-}
